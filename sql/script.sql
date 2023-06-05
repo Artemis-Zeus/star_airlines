6616a91bceb75538c0137dc404900dabb8bdeab1
Artemis <77585444+Believe-or-never@users.noreply.github.com>
2023-06-05 23:23:56 +0800

    User&Search Finish

---
+++ sql/script.sql
@@ -0,0 +1,61 @@
+create table car
+(
+    id      int auto_increment
+        primary key,
+    address varchar(20) not null,
+    price   int         not null,
+    brand   varchar(20) not null
+);
+
+create table flights
+(
+    id           int         null,
+    depart       varchar(20) null,
+    arrival      varchar(20) null,
+    depart_time  datetime    null,
+    arrival_time datetime    null,
+    price        int         null,
+    type         int         null comment '1-经济舱 2-商务舱 3-头等舱'
+);
+
+create table hotel
+(
+    id      int auto_increment
+        primary key,
+    address varchar(20) not null,
+    price   int         not null,
+    name    varchar(20) not null
+);
+
+create table user
+(
+    id       int auto_increment
+        primary key,
+    username varchar(20)   not null,
+    password varchar(20)   not null,
+    age      int           null,
+    address  varchar(30)   null,
+    email    varchar(20)   not null,
+    phone    varchar(20)   null,
+    card_num varchar(20)   null,
+    point    int default 0 null
+);
+
+create table record
+(
+    id          varchar(100)  not null comment '订单号，UUID'
+        primary key,
+    user_id     int           not null,
+    flight_id   int           not null,
+    hotel_id    int           not null,
+    days        int default 1 not null,
+    car_id      int           null,
+    price       int           null,
+    type        int           not null comment 'Flight:1-book 2-Refund',
+    update_time datetime      not null,
+    use_point   int           null comment '0-不用积分 1-使用积分（-500）',
+    constraint user_id
+        foreign key (user_id) references user (id)
+);
+
+
--- src/main/java/com/star_airlines/service/impl/SearchServiceImpl.java
+++ src/main/java/com/star_airlines/service/impl/SearchServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Service
